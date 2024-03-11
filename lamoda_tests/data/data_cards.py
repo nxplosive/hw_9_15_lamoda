e680d11a89f71b1047074cb5e65e9a17d4207b48
Nikita Safonov <148264012+nxplosive@users.noreply.github.com>
2024-03-11 10:06:12 +0300

    Amen

--- lamoda_tests/data/data_cards.py
+++ lamoda_tests/data/data_cards.py
@@ -13,13 +13,12 @@ class Card:
 card = Card(
     card_name='Кроссовки GALAXY STAR M',
     brand_name='adidas',
-    card_price='9 999 ₽',
-    item_name='Кроссовки adidas\n38 RUS (6 UK)\nСерый\n1\n9 999 ₽',
-    not_exist_data='j23hfg554'
+    card_price='5 600 ₽',
+    item_name='Кроссовки adidas\n38 RUS (6 UK)\nСерый',
+not_exist_data = 'j23hfg554'
 )
-
 
 @dataclasses.dataclass
 class Texts:
     nothing_found: str = 'Ничего не нашли по запросу'
-    empty_cart: str = 'В корзине нет товаров'
+    empty_cart: str = 'Корзина пока пустая'
